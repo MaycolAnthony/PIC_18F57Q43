484b2d65f9badfc31d7dac84a2fc54c2053e31ec
Maycol Anthony <maycolanthony2412@hotmail.com>
2022-10-04 14:55:38 -0500

    GPIO

---
+++ Learning/Primeros_pasos.X/config.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   config.h
+ * Author: maycol
+ *
+ * Created on 4 de octubre de 2022, 12:46 PM
+ */
+
+#ifndef CONFIG_H
+#define	CONFIG_H
+
+#define _XTAL_FREQ 64000000UL//Unsigned long
+void Clock_Config(void);
+
+#endif	/* CONFIG_H */
+
---
+++ Learning/Primeros_pasos.X/main.c
@@ -0,0 +1,20 @@
+/*
+ * File:   main.c
+ * Author: mayco
+ *
+ * Created on 4 de octubre de 2022, 12:47 PM
+ */
+
+
+#include <xc.h>
+#include<stdint.h>
+#include<stdio.h>
+#include"config.h"
+
+void main(void) {
+    Clock_Config();
+    while(1){
+        
+    }
+    return;
+}
